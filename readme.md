3a86403a921d68658ea8f78ac9ecb261582a66b9
Vladimír Ctibor <vladimir.ctibor@avast.com>
2021-01-27 21:14:58 +0100

    Something

---
+++ readme.md
@@ -0,0 +1,27 @@
+Colors:
+ 
+predefined:
+ - black
+ - white
+ - red
+ - yellow
+ - blue
+ - green
+
+rgb:
+    rgb(0, 15, 88)
+
+Comments:
+- single line: //
+- part-line / multiline: /* */
+
+comments should be nesting
+/* /* */ */
+
+/* */ */
+
+/* /* */
+
+anything inside comments shouldn't break compilation
+
+line x=p(0, 10) y=(25, 50) color=black;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use nom::IResult;
 use nom::bytes::complete::{is_a, is_not};
 use nom::character::is_digit;
-
+use nom::bytes::complete::tag;
+use nom::named;
+use nom::map;
 
 #[derive(Debug, PartialEq)]
 enum Token {
@@ -15,6 +17,7 @@ enum Token {
     Identifier(String),
 }
 
+/*
 type TokenStream = Vec<Token>;
 
 // vrací tuple, první hodnota je zbytek stringu, druhá hodnota je ta naparsovaná
@@ -177,13 +180,40 @@ fn lexer(input: &str) -> TokenStream {
 
     stream
 }
+*/
 
 fn main() {
     
     // IDENTIFIER(line) IDENTIFIER(x) EQUAL IDENTIFIER(p) PARENTHESES_START LITERAL(0) ...
     let input = "line x=p(0, 10) y=(25, 50) color=black;";
+    //let lexeme = lexer(input);
+    //println!("{:?}", lexeme);
 
-    let lexeme = lexer(input);
+    
 
-    println!("{:?}", lexeme);
+    named!(match_space<&str, Token>, map!(is_a(" \t\r\n"), |_| Token::Space));
+    named!(match_comma<&str, Token>, map!(tag(","), |_| Token::Comma));
+    named!(match_equal<&str, Token>, map!(tag("="), |_| Token::Equal));
+    named!(match_parenthesis_left<&str, Token>, map!(tag("("), |_| Token::ParenthesisLeft));
+    named!(match_parenthesis_right<&str, Token>, map!(tag(")"), |_| Token::ParenthesisRight));
+    named!(match_semicolon<&str, Token>, map!(tag(";"), |_| Token::Semicolon));
+    named!(match_identifier<&str, Token>, map!(is_not(" \t\r\n=,();"), |val| Token::Identifier(val.to_string())));
+
+
+    use nom::branch::alt;
+    use nom::combinator::all_consuming;
+
+    let mut match_anything = alt((
+        match_space,
+        match_comma,
+        match_equal,
+        match_parenthesis_left,
+        match_parenthesis_right,
+        match_semicolon,
+        match_identifier
+    ));
+
+    let res = match_anything(input);
+
+    println!("{:?}", res);
 }
