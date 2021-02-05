95fa5d348363e6279e6a47dc15bd722fd781952f
Vladimír Ctibor <vladimir.ctibor@avast.com>
2021-02-05 12:38:38 +0100

    Operation support

--- src/evaluation/helpers.rs
+++ src/evaluation/helpers.rs
@@ -18,42 +18,73 @@ pub struct Function {
 pub type FunMap = HashMap<String, Function>;
 
 pub fn create_arg_map(arg_vec: Vec<ast::Arg>, constants: &Constants) -> Result<ArgsMap> {
-    
-    use ast::OperationInput::*;
-
     let mut arg_map = HashMap::new();
-
     for arg in arg_vec {
-        
         let ast::Arg { name: arg_name, value } = arg;
-
-        match value {
-            Constant(const_name) => {
-                match  get_constant(&const_name, &constants) {
-                    Some(val) => { arg_map.insert(arg_name, val.clone()); },
-                    None => return Err(anyhow!("Unknown constant {}", &const_name))
-                }
-            }
-
-            Literal(val) => {
-                arg_map.insert(arg_name, val);
-            }
-
-            Operation(_) => {
-                
-            }
-        }
+        let constant = get_value(&value, constants)?;
+        arg_map.insert(arg_name, constant);
     }
-
     Ok(arg_map)
 }
 
-fn get_constant(name: &str, const_vec: &Constants) -> Option<ast::Value> {
-    for const_map in const_vec {
+fn get_constant(name: &str, constants: &Constants) -> Result<ast::Value> {
+    for const_map in constants {
         if let Some(constant) = const_map.get(name) {
-            return Some(constant.clone())
+            return Ok(constant.clone())
         }
     }
 
-    None
+    Err(anyhow!("Unknown constant {}", &name))
+}
+
+fn eval_operation(operation: ast::Operation, constants: &Constants) -> Result<ast::Value> {
+
+    use ast::Operator::*;
+
+    let ast::Operation { lh, op, rh } = operation;
+
+    let left_hand_value: ast::Value = get_value(lh.as_ref(), constants).unwrap();
+    let right_hand_value: ast::Value = get_value(rh.as_ref(), constants).unwrap();
+    
+    match op {
+        Addition => {
+            panic!("Not yet implemented!");
+        },
+        Subtraction => {
+            panic!("Not yet implemented!");
+        },
+        Multiplication => {
+            panic!("Not yet implemented!");
+        },
+        Division => {
+            panic!("Not yet implemented!");
+        },
+        LessThan => {
+            panic!("Not yet implemented!");
+        },
+        GreaterThan => {
+            panic!("Not yet implemented!");
+        },
+        Equality => {
+            panic!("Not yet implemented!");
+        },
+        LogicalAnd => {
+            panic!("Not yet implemented!");
+        },
+        LogicalOr => {
+            panic!("Not yet implemented!");
+        },
+        Negation => {
+            panic!("Not yet implemented!");
+        },
+    }
+}
+
+fn get_value(input: &ast::OperationInput, constants: &Constants) -> Result<ast::Value> {
+    use ast::OperationInput::*;
+    match input {
+        Literal(value) => Ok(value.clone()),
+        Constant(name) => get_constant(&name, constants),
+        Operation(op) => eval_operation(op.clone(), constants),
+    }
 }
