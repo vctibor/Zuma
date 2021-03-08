9994fccfd20298126b5a22a792621b43551787ae
Vladimír Ctibor <vladimir.ctibor@avast.com>
2021-03-08 23:56:43 +0100

    CLI interface

---
+++ vscode_extension/README.md
@@ -0,0 +1,9 @@
+# ZUMA Visual Studio Code extension
+
+This is an attempt to create ZUMA extension for Visual Studio Code.
+
+Goals are:
+
+- code highlighting
+
+- preview of generated SVG or PNG
--- zuma/src/main.rs
+++ zuma/src/main.rs
@@ -1,64 +1,104 @@
 use std::fs::{read_to_string, File};
 use std::time::Instant;
 use std::io::Write;
-use std::{thread, time};
+use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
+use structopt::StructOpt;
 
-/*
-fn render_svg(svg: String, path: &str) {
-
-    use resvg::render;
-    use usvg::{Tree, Options, FitTo};
-    use tiny_skia::Pixmap;
-
-    let mut options = Options::default();
-    options.font_family = "Fira Code".to_owned();
+/// ZUMA language compiler.
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "zuma",
+    about = "ZUMA language compiler",
+    rename_all = "kebab-case"
+)]
+struct Args {
     
-    let tree = Tree::from_str(&svg, &options).unwrap();
-    let mut pixmap = Pixmap::new(1000, 1000).unwrap();
-    let pixmap_mut = pixmap.as_mut();
-    render(&tree, FitTo::Original, pixmap_mut);
-    pixmap.save_png(path).unwrap();
+    /// Don't exit, recompile whenever source changes.
+    #[structopt(short, long)]
+    watch: bool,
+
+    /// Create .png output file.
+    #[structopt(long, parse(try_from_str), default_value="false")]
+    png: bool,
+
+    /// Create .svg output file. 
+    #[structopt(long, parse(try_from_str), default_value="true")]
+    svg: bool,
+
+    /// Output to STDOUT.
+    #[structopt(long, parse(try_from_str), default_value="false")]
+    stdout: bool,
+
+    /// Path to source file.
+    #[structopt(parse(from_os_str))]
+    filename: PathBuf,
+
+    /// Optional custom name for .png output.
+    /// Implies png = true.
+    #[structopt(parse(from_os_str))]
+    png_filename: Option<PathBuf>,
+
+    /// Optional custom name for .svg output.
+    /// Implies svg = true.
+    #[structopt(parse(from_os_str))]
+    svg_filename: Option<PathBuf>,
 }
-*/
 
-fn compile_file(compiler: &zumalib::ZumaCompiler) -> Result<()>
-{
-    let folder = "examples";
-    //let input_file = "front_page01";
-    let input_file = "example01";
+#[paw::main]
+fn main(args: Args) -> Result<()> {
 
-    let zuma_input = format!("{}/{}.zm", folder, input_file);
-    let svg_output = format!("{}/{}.svg", folder, input_file);
-    //let png_output = format!("{}/{}.png", folder, input_file);
+    let file_stem = args.filename.file_stem().ok_or(anyhow!("Invalid filename!"))?;
+
+    let svg_filename = {
+        let mut svg_filename = file_stem.clone().to_owned();
+        svg_filename.push(".svg");
+        args.svg_filename.clone().unwrap_or(PathBuf::from(svg_filename))
+    };
+
+    let png_filename = {
+        let mut png_filename = file_stem.clone().to_owned();
+        png_filename.push(".png");
+        args.png_filename.clone().unwrap_or(PathBuf::from(png_filename))
+    };
 
     let start_time = Instant::now();
-    let input = read_to_string(zuma_input)?;
-    let svg = compiler.compile(input)?;
-    let mut output_file = File::create(svg_output)?;
-    output_file.write_all(svg.as_bytes())?;
 
-    //render_svg(svg, &png_output);
+    let compiler = zumalib::ZumaCompiler::new();
+
+    let input = read_to_string(args.filename)?;
+    let svg = compiler.compile(input)?;
+
+    if args.stdout {
+        println!("{}", svg);
+    }
+
+    if args.svg || args.svg_filename.is_some() {
+        let mut output_file = File::create(svg_filename)?;
+        output_file.write_all(svg.as_bytes())?;
+    }
+
+    if args.png || args.png_filename.is_some() {
+        println!("Sorry, PNG rendering currently isn't available!");
+        /*
+        use resvg::render;
+        use usvg::{Tree, Options, FitTo};
+        use tiny_skia::Pixmap;
+
+        let mut options = Options::default();
+        options.font_family = "Fira Code".to_owned();
+        
+        let tree = Tree::from_str(&svg, &options).unwrap();
+        let mut pixmap = Pixmap::new(1000, 1000).unwrap();
+        let pixmap_mut = pixmap.as_mut();
+        render(&tree, FitTo::Original, pixmap_mut);
+        pixmap.save_png(path).unwrap();
+        */
+    }
 
     let end_time = start_time.elapsed();
     println!("{:?}", end_time);
 
     Ok(())
-}
-
-fn main() {
-
-    let compiler = zumalib::ZumaCompiler::new();
-
-    loop {
-        thread::sleep(time::Duration::from_millis(10));
-        let res = compile_file(&compiler);
-
-        println!("{:?}", res);
-        
-        println!("");
-
-        //break;
-    }
 }
